fe79598fb948b20b0c4f330c5dba8942f20eea4a
tlagoda <thibautlagoda@hotmail.fr>
2023-05-10 23:48:19 +0200

    init addPerformance compoenent

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import MyHome from './views/MyHome.vue'
 import SignUp from './views/SignUp.vue'
 import SignIn from './views/SignIn.vue'
+import AddPerformance from './views/AddPerformance.vue'
 
 export const routes = [
   {
@@ -17,5 +18,10 @@ export const routes = [
     path: '/signin',
     name: 'signin',
     component: SignIn
+  },
+  {
+    path: '/performances/add',
+    name: 'addPerformance',
+    component: AddPerformance
   }
 ]

a45974010982cfd9b69c27a419fb7ddf2dc9fc3e
tlagoda <thibautlagoda@hotmail.fr>
2023-05-16 00:21:57 +0200

    signup page v1

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,13 +1,24 @@
 <template>
   <h1>Create your account! 🚀</h1>
   <v-form ref="form" @submit.prevent="signUp">
-    <v-text-field label="Email *" required :rules="[rules.required]"></v-text-field>
-    <v-text-field label="Username *" required :rules="[rules.required]"></v-text-field>
+    <v-text-field
+      label="Email *"
+      required
+      :rules="[rules.required]"
+      v-model="newUser.email"
+    ></v-text-field>
+    <v-text-field
+      label="Username *"
+      required
+      :rules="[rules.required]"
+      v-model="newUser.username"
+    ></v-text-field>
     <v-text-field
       label="Password *"
       type="password"
       required
       :rules="[rules.required]"
+      v-model="newUser.password"
     ></v-text-field>
     <v-text-field
       label="Confirm Password *"
@@ -15,20 +26,36 @@
       required
       :rules="[rules.required]"
     ></v-text-field>
+    <p v-if="signUpError" class="error-message">{{ signUpError }}</p>
     <v-btn type="submit" color="red">Sign up</v-btn>
   </v-form>
 </template>
 
 <script lang="ts" setup>
+import { AuthService } from '@/services/auth.service.'
+import type { NewUser } from '@/types/auth/types'
+import { reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
+
+const newUser: NewUser = reactive({
+  username: '',
+  email: '',
+  password: '',
+  sexe: 'M'
+})
+const signUpError = ref('')
 
 const rules = {
   required: (value: string) => !!value || 'Required.'
 }
-const signUp = () => {
-  console.log('User signed up.')
-  router.push({ path: '/' })
+const signUp = async () => {
+  try {
+    const response = await AuthService.signup(newUser)
+    
+  } catch (err: any) {
+    signUpError.value = err.message
+  }
 }
 </script>
 
@@ -43,5 +70,9 @@ h1 {
 
 .v-btn {
   width: 45%;
+}
+
+.error-message {
+  color: red;
 }
 </style>

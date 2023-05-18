76dd5c249d07ca6dfebe1ad64f6bb7d4938dca67
tlagoda <thibautlagoda@hotmail.fr>
2023-05-18 17:38:08 +0200

    fix getAll perf

--- src/services/performances.service.ts
+++ src/services/performances.service.ts
@@ -5,10 +5,12 @@ import type { PerformanceFilter } from '@/types/performances/types'
 export class PerformanceService {
   static async getAll(perfFilter: PerformanceFilter) {
     try {
-      const response = await axios.get(`${URLs.BACK_URL}/performance`)
+      const response = await axios.get(`${URLs.BACK_URL}/performance/filters`)
       return response
     } catch (err: any) {
       throw err.response.data
     }
   }
+
+  static async createPerformance() {}
 }

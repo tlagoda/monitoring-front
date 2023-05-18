fa79f24134b3bc77f936d43dd2268626fff8247a
tlagoda <thibautlagoda@hotmail.fr>
2023-05-18 22:35:36 +0200

    improve getAll and getFilters perf

--- src/services/performances.service.ts
+++ src/services/performances.service.ts
@@ -4,20 +4,24 @@ import type { PerformanceFilter } from '@/types/performances/types'
 
 export class PerformanceService {
   static async getAll() {
-    // A TESTER
     try {
       const response = await axios.get(`${URLs.BACK_URL}/performance`)
-      return response
+      return {
+        statusCode: response.status,
+        data: response.data
+      }
     } catch (err: any) {
       throw err.response.data
     }
   }
 
   static async getFilteredPerformances(filters: PerformanceFilter) {
-    // A TESTER
     try {
-      const response = await axios.get(`${URLs.BACK_URL}/performance/filters`, { data: filters })
-      return response
+      const response = await axios.post(`${URLs.BACK_URL}/performance/filters`, filters)
+      return {
+        statusCode: response.status,
+        data: response.data
+      }
     } catch (err: any) {
       throw err.response.data
     }

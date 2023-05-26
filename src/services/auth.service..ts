6585ddb3de55f8f27fab85bddd8c3b2c672448ef
tlagoda <thibautlagoda@hotmail.fr>
2023-05-26 19:24:02 +0200

    fix signup method in AuthService

--- src/services/auth.service..ts
+++ src/services/auth.service..ts
@@ -6,9 +6,14 @@ export class AuthService {
   static async signup(newUser: NewUser) {
     try {
       const response = await axios.post(`${URLs.BACK_URL}/auth/signup`, newUser)
+
+      if (!response) {
+        throw new Error('No response received from the server.')
+      }
+
       return {
-        statusCode: response.status,
-        data: response.data
+        statusCode: response?.status,
+        data: response?.data
       }
     } catch (err: any) {
       throw err.response.data

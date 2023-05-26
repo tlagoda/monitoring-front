13fb341f2830270185a65cf23fffa6086a8f3219
tlagoda <thibautlagoda@hotmail.fr>
2023-05-26 21:31:37 +0200

    add perf

--- src/views/AddPerformance.vue
+++ src/views/AddPerformance.vue
@@ -1,8 +1,12 @@
 <template>
   <h1>Add a performance! 💪</h1>
-  <Card class="uu">
+  <Card>
     <template #content>
-      <InputText />
+      <div class="content">
+        <InputText />
+        <InputText />
+        <InputText />
+      </div>
     </template>
   </Card>
 </template>
@@ -12,11 +16,21 @@ import Card from 'primevue/card'
 import InputText from 'primevue/inputtext'
 </script>
 
-<style scoped>
-.uu {
+<style scoped lang="scss">
+h1 {
+  text-align: center;
+  margin: 2rem 0;
+}
+.p-card {
   width: 40vw;
   margin: 0 auto;
   height: 80vh;
   border-radius: 2rem;
+  border: 1px solid white;
+
+  .content {
+    display: flex;
+    flex-direction: column;
+  }
 }
 </style>

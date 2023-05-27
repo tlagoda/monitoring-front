62cfaf15583235f7ae43f0498244c5f584ea1da4
tlagoda <thibautlagoda@hotmail.fr>
2023-05-27 10:12:13 +0200

    fix global layout

--- src/views/AddPerformance.vue
+++ src/views/AddPerformance.vue
@@ -18,19 +18,19 @@
           </div>
           <div class="input-section">
             <label for="">Sets</label>
-            <InputText />
+            <InputNumber :inputStyle="{ width: '12rem'}"/>
           </div>
           <div class="input-section">
             <label for="">Repetitions</label>
-            <InputText />
+            <InputNumber :inputStyle="{ width: '12rem'}"/>
           </div>
           <div class="input-section">
             <label for="">RestTime</label>
-            <InputText />
+            <InputNumber :inputStyle="{ width: '12rem'}"/>
           </div>
           <div class="input-section">
             <label for="">Weight</label>
-            <InputText />
+            <InputNumber :inputStyle="{ width: '12rem'}" suffix=" lbs" />
           </div>
           <div class="input-section">
             <label for="">Comment</label>
@@ -47,6 +47,7 @@
 import Card from 'primevue/card'
 import Calendar from 'primevue/calendar'
 import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
 import MultiSelect from 'primevue/multiselect'
 import Textarea from 'primevue/textarea'
 import Button from 'primevue/button'
@@ -61,26 +62,38 @@ h1 {
 }
 .p-card {
   width: 40vw;
+  min-width: 30rem;
   margin: 0 auto;
   height: 80vh;
   border-radius: 2rem;
   border: 1px solid white;
 
   .content {
-    display: flex;
-    flex-direction: column;
-
+    form {
+      display: flex;
+      flex-direction: column;
+    }
     .input-section {
-      width: 60%;
+      width: 80%;
       margin: 0.5rem auto;
       display: flex;
       justify-content: space-between;
       align-items: center;
 
       label {
-        margin-right: 1rem;
         font-size: 1.3rem;
       }
+
+      & > :last-child {
+        width: 15rem;
+      }
+    }
+
+    :deep(.p-button) {
+      font-size: 1.3rem;
+      padding: 0.5rem 1.5rem;
+      margin: 1rem auto;
+      border-radius: 1rem;
     }
   }
 }

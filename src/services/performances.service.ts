944087b10203cbec134f866fd497fc62213aa86d
tlagoda <thibautlagoda@hotmail.fr>
2023-05-27 11:52:19 +0200

    scaffold function

--- src/services/performances.service.ts
+++ src/services/performances.service.ts
@@ -1,6 +1,6 @@
 import { URLs } from '@/config/config'
 import axios from 'axios'
-import type { PerformanceFilter } from '@/types/performances/types'
+import type { PerformanceFilter, PerformanceCreate } from '@/types/performances/types'
 
 export class PerformanceService {
   static async getAll() {
@@ -27,7 +27,15 @@ export class PerformanceService {
     }
   }
 
-  static async createPerformance() {
-    
+  static async createPerformance(performance: PerformanceCreate) {
+    try {
+      const response = await axios.post(`${URLs.BACK_URL}/performance/`, performance)
+      return {
+        statusCode: response.status,
+        data: response.data
+      }
+    } catch (err: any) {
+      throw err.response.data
+    }
   }
 }
